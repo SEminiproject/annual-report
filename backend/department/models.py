801770e749126a80e2c7c06a5254fc437533c2f6
rushaad29 <m.rushaadq@gmail.com>
2025-05-09 14:14:26 +0530

    created a simple Department schema along with  student and staff apps

--- backend/department/models.py
+++ backend/department/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from ..college.models import College
+from ..staff.models import Staff
+import uuid
 
-# Create your models here.
+
+
+class Deparment(models.Model):
+    
+    id = models.UUIDField(default=uuid.uuid4,primary_key=True,null=False,blank=False)
+    name = models.CharField(max_length=100,null=False,null=False)
+    college = models.ForeignKey(College,on_delete=models.CASCADE,related_name='dept_college')
+    hod = models.ForeignKey(Staff,blank=True,null=True)
+    
